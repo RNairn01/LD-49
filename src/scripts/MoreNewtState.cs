2312fed2b869c677380efbaf9daa8bc75a0357da
Russell <74921709+RNairn01@users.noreply.github.com>
2021-10-02 17:23:31 +0100

    Add newt dropping

--- src/scripts/MoreNewtState.cs
+++ src/scripts/MoreNewtState.cs
@@ -4,9 +4,11 @@ using System;
 public class MoreNewtState : AlchemyInput, IAlchemyInput
 {
     private bool holdingNewt, newtIsFalling, newtCanBeDropped = false;
+    private Vector2 startPosition;
     public override void _Ready()
     {
         base._Ready();
+        startPosition = GetNode<Node2D>("../NewtJar").GlobalPosition;
         inputState = InputStates.InputState.StirState;
     }
 
@@ -17,6 +19,9 @@ public class MoreNewtState : AlchemyInput, IAlchemyInput
         {
             GlobalPosition = Drag(GlobalPosition, GetGlobalMousePosition(), 25 * delta);
         }
+
+        if (newtIsFalling)
+            GlobalPosition = Drag(GlobalPosition, new Vector2(GlobalPosition.x, GlobalPosition.y + 100), 15 * delta);
     }
 
     public void OnInteract()
@@ -80,13 +85,23 @@ public class MoreNewtState : AlchemyInput, IAlchemyInput
 
     public void OnNewtCollide(Area2D area)
     {
+        GD.Print("Newt collided with: " + area.Name);
+        
         if (area.Name == "NewtDrop")
         {
             GD.Print("Newt dropped in cauldron!");
+            if (holdingNewt) return;
+            GlobalPosition = startPosition;
+            newtIsFalling = false;
             OnComplete();
         }
 
-        if (area.Name == "Catcher") OnFailure();
+        if (area.Name == "Catcher")
+        {
+            GlobalPosition = startPosition;
+            newtIsFalling = false;
+            OnFailure();
+        }
     }
 
     private void DropNewt()
